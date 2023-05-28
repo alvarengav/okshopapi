2a32bf87b78c4f241ce12d7992438088d0109c87
Daniel Jimenez <djimenezsoza18@gmail.com>
2023-05-28 09:32:55 -0600

    Added authentication and strong password validation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,36 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Validator::extend('strong_password', function ($attribute, $value, $parameters, $validator) {
+            $minLength = 8;
+            $uppercaseRequired = true;
+            $lowercaseRequired = true;
+            $numberRequired = true;
+            $specialCharRequired = true;
+
+            $regex = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/";
+
+            if (strlen($value) < $minLength) {
+                return false;
+            }
+
+            if ($uppercaseRequired && !preg_match('/[A-Z]/', $value)) {
+                return false;
+            }
+
+            if ($lowercaseRequired && !preg_match('/[a-z]/', $value)) {
+                return false;
+            }
+
+            if ($numberRequired && !preg_match('/\d/', $value)) {
+                return false;
+            }
+
+            if ($specialCharRequired && !preg_match('/[@$!%*?&]/', $value)) {
+                return false;
+            }
+
+            return true;
+        }, "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.");
     }
 }
