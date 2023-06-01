56f65dc1eb4f28afb0bc88fdfa2876401b5a96ae
Daniel Jimenez <djimenezsoza18@gmail.com>
2023-06-01 06:38:17 -0600

    Updated password field of the password_manager table

--- database/migrations/2023_05_30_161338_create_password_manager_table.php
+++ database/migrations/2023_05_30_161338_create_password_manager_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('social_network_name');
             $table->string('url');
             $table->string('username');
-            $table->string('password');
+            $table->longText('password');
             $table->timestamps();
 
             $table->foreignId('user_id')->constrained();
