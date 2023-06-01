386abd345cfd3b6439604e6be0ea32fe47c4552c
Daniel Jimenez <djimenezsoza18@gmail.com>
2023-06-01 06:30:11 -0600

    Added index lenght to mysql

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
         Validator::extend('strong_password', function ($attribute, $value, $parameters, $validator) {
             $minLength = 8;
             $uppercaseRequired = true;
