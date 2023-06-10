bef6919dc07c044e31550174e0eeaac9b8c65a96
Daniel Jimenez <djimenezsoza18@gmail.com>
2023-06-10 08:01:29 -0600

    Updated upload image method

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -62,14 +62,8 @@ class ProductController extends Controller
             return response()->json(['error' => 'Invalid image'], 400);
         }
 
-        //$path = $image->storeAs('product_images', $image->getClientOriginalName(), 'public');
-        // $image->move(base_path(''))
-        // // $url = asset('storage/' . $path);
-
-        // This is to work on webhost
-        $image->move(base_path('/public_html/images/products/'), $image->getClientOriginalName());
-        $url = url('/images/products/' . $image->getClientOriginalName());
-        $product->image_url = $url;
+        $path = $image->storeAs('product_images', $image->getClientOriginalName(), 'public');
+        $product->image_url = $path;
         $product->update();
         return response()->json(['message' => 'Image uploaded successfully'], 200);
     }
